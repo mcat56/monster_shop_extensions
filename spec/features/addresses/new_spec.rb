0fab4720949f29506878381e94153b5ddb6398d0
Mary Lang <mcl86@cornell.edu>
2019-11-02 14:40:33 -0600

    Add to address new spec

--- spec/features/addresses/new_spec.rb
+++ spec/features/addresses/new_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 
 describe 'users can add a new address from their profile page' do
-  it 'user links from profile to fill in new address form' do
-
+  before(:each) do
     visit '/'
-
-    user = User.create(name: 'Patti', email: 'pattimonkey34@gmail.com', password: 'banana')
-
-
+    @user = User.create(name: 'Patti', email: 'pattimonkey34@gmail.com', password: 'banana')
     click_link 'Login'
 
-    fill_in :email, with: user.email
-    fill_in :password, with: user.password
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
     click_button 'Log In'
 
     click_link 'Add an Address'
+  end
+  it 'user links from profile to fill in new address form' do
+    expect(current_path).to eq("/users/#{@user.id}/addresses/new")
 
     fill_in 'Nickname', with: 'work'
     fill_in 'Address', with: '28 Park Ave'
@@ -24,9 +23,9 @@ describe 'users can add a new address from their profile page' do
     fill_in 'Zip', with: '19103'
     click_button 'Create Address'
 
-    expect(current_path).to eq("/profile/#{user.id}")
+    expect(current_path).to eq("/profile/#{@user.id}")
     expect(page).to have_content('Your address has been added.')
-    address = user.addresses.last
+    address = @user.addresses.last
 
     within "#address-#{address.id}" do
       expect(page).to have_content('work')
@@ -35,6 +34,18 @@ describe 'users can add a new address from their profile page' do
       expect(page).to have_content('State: PA')
       expect(page).to have_content('Zip Code: 19103')
     end
+  end
+  it 'user cannot create address without filling out all fields' do
+    expect(current_path).to eq("/users/#{@user.id}/addresses/new")
 
+    fill_in 'Nickname', with: ''
+    fill_in 'Address', with: '28 Park Ave'
+    fill_in 'City', with: 'Philadelphia'
+    fill_in 'State', with: 'PA'
+    fill_in 'Zip', with: ''
+    click_button 'Create Address'
+
+    expect(current_path).to eq("/users/#{@user.id}/addresses/new")
+    expect(page).to have_content("Nickname can't be blank and Zip can't be blank")
   end
 end
